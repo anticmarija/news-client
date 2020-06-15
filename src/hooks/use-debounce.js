c2622cf00f7c52b9f1c58a613282a978b9e64466
Marija Antic <m.antic@levi9.com>
2020-06-15 23:14:39 +0200

    chore(search): lint

---
+++ src/hooks/use-debounce.js
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+export default function useDebounce(value, timeout) {
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, timeout);
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value]);
+
+  return debouncedValue;
+}
